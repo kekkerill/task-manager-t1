7ac7168ddb266e98a22834a3cdf343415a429f61
kekkerill <kirill232348@yandex.com>
2025-07-15 13:29:37 +0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@/": "/src/",
-      "@entities/": "/src/entities/",
-      "@features/": "/src/features/",
-      "@widgets/": "/src/widgets/",
-      "@shared/": "/src/shared/",
-      "@pages/": "/src/pages/",
-      "@app/": "/src/app/",
-      "@config/": "/src/config/"
+      "@/": path.resolve(__dirname, "src"),
+      "@entities": path.resolve(__dirname, "src/entities"),
+      "@features": path.resolve(__dirname, "src/features"),
+      "@widgets": path.resolve(__dirname, "src/widgets"),
+      "@shared": path.resolve(__dirname, "src/shared"),
+      "@pages": path.resolve(__dirname, "src/pages"),
+      "@app": path.resolve(__dirname, "src/app"),
+      "@config": path.resolve(__dirname, "src/config")
     }
   }
 });
